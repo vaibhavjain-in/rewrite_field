9448af4f3cbca5467f2988605f28de53d23cbafc
Vaibhav Jain <in.vaibhavjain@gmail.com>
2016-06-03 13:00:31 +0530

    Added code to accept Link options and attributes. Also added support for text case transform.

--- src/Plugin/Field/FieldFormatter/RewriteFieldFormatter.php
+++ src/Plugin/Field/FieldFormatter/RewriteFieldFormatter.php
@@ -12,6 +12,7 @@ use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Url;
 use Drupal\Core\Link;
+use Drupal\Component\Utility\Unicode;
 
 /**
  * Plugin implementation of the 'prefix_suffix' formatter.
@@ -32,7 +33,9 @@ use Drupal\Core\Link;
  *     "make_link" = FALSE,
  *     "link_path" = "",
  *     "external_link" = FALSE,
- *     "absolute_link" = FALSE,
+ *     "link_options" = "",
+ *     "link_attributes" = "",
+ *     "text_case" = ""
  *   }
  * )
  */
@@ -49,7 +52,9 @@ class RewriteFieldFormatter extends FormatterBase {
       'make_link' => FALSE,
       'link_path' => '',
       'external_link' => FALSE,
-      'absolute_link' => FALSE,
+      'link_options' => '',
+      'link_attributes' => '',
+      'text_case' => 'none'
     ) + parent::defaultSettings();
   }
 
@@ -91,7 +96,7 @@ class RewriteFieldFormatter extends FormatterBase {
       '#title' => t('Link path'),
       '#type' => 'textfield',
       '#default_value' => $this->settings['link_path'],
-      '#description' => $this->t('The Drupal path or absolute URL for this link.'),
+      '#description' => $this->t('The Drupal path (/node) or absolute URL (http://www.example.com) for this link.'),
       '#states' => array(
         'visible' => array(
           ':input[name$="[settings_edit_form][settings][make_link]"]' => array('checked' => TRUE),
@@ -112,11 +117,11 @@ class RewriteFieldFormatter extends FormatterBase {
       ),
     );
 
-    $element['absolute_link'] = array(
-      '#type' => 'checkbox',
-      '#title' => t('Absolute path'),
-      '#default_value' => $this->settings['absolute_link'],
-      '#description' => $this->t("Generate Absolute link"),
+    $element['link_options'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Link Options'),
+      '#default_value' => $this->settings['link_options'],
+      '#description' => $this->t("Comma seperated list of options. Like, query=key|value,fragment=1234,absolute=true"),
       '#states' => array(
         'visible' => array(
           ':input[name$="[settings_edit_form][settings][make_link]"]' => array('checked' => TRUE),
@@ -124,8 +129,32 @@ class RewriteFieldFormatter extends FormatterBase {
       ),
     );
 
-    // @todo: Link title and target
-    // @todo: Text Transform(Lower, upper), No Results behaviour
+    $element['link_attributes'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Link Attributes'),
+      '#default_value' => $this->settings['link_attributes'],
+      '#description' => $this->t("Comma seperated list of attributes. Like, class=myClass1 myClass2,rel=lightbox,target=_blank"),
+      '#states' => array(
+        'visible' => array(
+          ':input[name$="[settings_edit_form][settings][make_link]"]' => array('checked' => TRUE),
+        ),
+      ),
+    );
+
+    $element['text_case'] = array(
+      '#type' => 'select',
+      '#title' => $this->t('Transform the text case'),
+      '#description' => $this->t('Transform the case of the text.'),
+      '#options' => array(
+        'none' => $this->t('No transform'),
+        'strtoupper' => $this->t('Upper case'),
+        'strtolower' => $this->t('Lower case'),
+        'ucfirst' => $this->t('Capitalize first letter'),
+        'ucwords' => $this->t('Capitalize each word'),
+      ),
+      '#default_value' => $this->settings['text_case'],
+    );
+
     return $element;
   }
 
@@ -138,8 +167,7 @@ class RewriteFieldFormatter extends FormatterBase {
     $suffix = $this->settings['suffix'];
     $custom_text = $this->settings['custom_text'];
     $link_path = strip_tags($this->settings['link_path']);
-    $external_link = (bool) $this->settings['external_link'];
-    $absolute_link = (bool) $this->settings['absolute_link'];
+    $text_case = $this->settings['text_case'];
     foreach ($items as $delta => $item) {
       $output = $item->value;
       if (!empty($output) && !empty($custom_text)) {
@@ -148,15 +176,11 @@ class RewriteFieldFormatter extends FormatterBase {
       else {
         // @todo: Add No Results Behaviour
       }
+      if ($text_case != 'none') {
+        $output = Unicode::$text_case($output);
+      }
       if ($this->settings['make_link'] && !empty($link_path)) {
-        if ($external_link) {
-          $url = Url::fromUri($link_path);
-        }
-        else {
-          $link_path = (strpos($link_path, '/') !== 0) ? '/' . $link_path : $link_path;
-          $url = Url::fromUserInput($link_path, array('absolute' => $absolute_link));
-        }
-        $output = Link::fromTextAndUrl($output, $url)->toString();
+        $output = $this->makeLink($link_path, $output);
       }
       if (!empty($prefix)) {
         $output = $prefix . $output;
@@ -169,6 +193,56 @@ class RewriteFieldFormatter extends FormatterBase {
       ];
     }
     return $elements;
+  }
+
+  /**
+   * Generate the output as a link, with inputs from user.
+   */
+  private function makeLink($link_path, $output) {
+    $link_options = array();
+    if (!empty($this->settings['link_options'])) {
+      $link_options = $this->createArray($this->settings['link_options']);
+    }
+    if (isset($this->settings['link_attributes'])) {
+      $link_options['attributes'] = $this->createArray($this->settings['link_attributes']);
+    }
+
+    if (!empty($this->settings['external_link'])) {
+      $url = Url::fromUri($link_path, $link_options);
+    }
+    else {
+      // Add a slash in front of user provided path.
+      $link_path = (strpos($link_path, '/') !== 0) ? '/' . $link_path : $link_path;
+      $url = Url::fromUserInput($link_path, $link_options);
+    }
+
+    return Link::fromTextAndUrl($output, $url)->toString();
+  }
+
+  /**
+   * Create the array, parsable to core methods.
+   */
+  private function createArray($string) {
+    $values = array();
+    $explodedValues = explode(',', $string);
+    foreach ($explodedValues as $explodedValue) {
+      $explodedVal = explode('=', $explodedValue);
+      // Handle "query" case.
+      if ($explodedVal[0] == 'query') {
+        $queryValues = explode('&', $explodedVal[1]);
+        foreach ($queryValues as $queryValue) {
+          $queryVal = explode('|', $queryValue);
+          $values[$explodedVal[0]][$queryVal[0]] = $queryVal[1];
+        }
+      }
+      elseif ($explodedVal[0] == 'class') {
+        $values[$explodedVal[0]] = explode(' ', $explodedVal[1]);
+      }
+      else {
+        $values[$explodedVal[0]] = $explodedVal[1];
+      }
+    }
+    return $values;
   }
 
 }
